0d391e0656b69810ebe1b2a001f22f8ba5978f11
Drup <drupyog@zoho.com>
2016-05-26 01:24:47 +0200

    Add some tests.

--- test/test.ml
+++ test/test.ml
@@ -1,18 +1,49 @@
+module A = struct
+  include Alcotest
+  let float = of_pp Format.pp_print_float
+end
 open Tyre
 
-let r =
-  compile (
-    "foo" *> separated_list ~sep:"," int <* "bar"
-  )
+let t' title desc re v s =
+  let cre = Tyre.compile re in
+  A.(check (option desc)) (title^" exec") (Tyre.exec cre s) (Some v) ;
+  A.(check string) (title^" eval") s (Tyre.eval re v)
 
-let test1 () =
-  Alcotest.(check @@ option @@ list int)
-    "1 elem" (exec r "foo3bar") @@ Some [3]
+let t title desc re v s =
+  title, `Quick, fun () -> t' title desc re v s
 
-let l = [
-  "int list", `Quick, test1
+let topt title desc re v s s' =
+  title, `Quick,
+  fun () ->
+    t' (title ^" some") (A.option desc) re (Some v) s ;
+    t' (title ^" none") (A.option desc) re None s'
+
+let basics = [
+  t "int" A.int int 42 "42" ;
+  t "int pos" A.int pos_int 549085 "549085" ;
+  t "int neg" A.int int (-54) "-54" ;
+
+  t "float int" A.float float 3. "3." ;
+  (* t "float_int2" A.float float 3. "3" ; *)
+  t "float" A.float float 4.2 "4.2" ;
+  t "float neg" A.float float (-4.2) "-4.2" ;
+
+  t "bool true" A.bool bool true "true" ;
+  t "bool false" A.bool bool false "false" ;
+]
+
+let prefix_suffix = [
+  t "prefix" A.int ("foo" *> int) 3 "foo3" ;
+  t "suffix" A.int (int <* "foo") 3 "3foo" ;
+]
+
+let composed = [
+  topt "option" A.int (opt int) 3 "3" "" ;
+  topt "option prefix" A.int (opt int <* "foo") 3 "3foo" "foo" ;
 ]
 
 let () = Alcotest.run "tyre" [
-    "test", l
+    "basics", basics ;
+    "prefix suffix", prefix_suffix ;
+    "composed", composed ;
   ]
