c88c47f8116d922d6479dca219724fd64a54cfd7
Drup <drupyog@zoho.com>
2016-07-05 22:49:48 +0200

    Add travis rune for auto-upload of dev doc.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,1 +1,36 @@
 bash -ex .travis-opam.sh
+
+## Documentation stuff
+
+set -e
+# Make sure we're not echoing any sensitive data
+set +x
+set -o errexit -o nounset
+
+eval `opam config env`
+./configure --enable-docs
+make doc
+
+if [ -z "$TRAVIS" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "This is not a push Travis-ci build, doing nothing..."
+  exit 0
+else
+  echo "Updating docs on Github pages..."
+fi
+
+DOCDIR=.gh-pages/
+if [ -n "$KEEP" ]; then trap "rm -rf $DOCDIR" EXIT; fi
+
+# Error out if $GH_TOKEN is empty or unset
+: ${GH_TOKEN:?"GH_TOKEN need to be uploaded via travis-encrypt"}
+
+make .gh-pages/.git 2>&1 | sed -e "s/$GH_TOKEN/!REDACTED!/g"
+
+rm -rf $DOCDIR/dev/*
+cp _build/*.docdir/* $DOCDIR/dev
+
+git -C $DOCDIR config user.email "travis@travis-ci.org"
+git -C $DOCDIR config user.name "Travis"
+git -C $DOCDIR add --all dev
+git -C $DOCDIR commit --allow-empty -m "Travis build $TRAVIS_BUILD_NUMBER pushed docs to gh-pages"
+git -C $DOCDIR push origin gh-pages 2>&1 | sed -e "s/$GH_TOKEN/!REDACTED!/g"
